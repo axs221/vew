e423dc9df39ca9c276e08e862f4f94b8ca0eb146
Shawn Axsom <shawn.axsom@rooksecurity.com>
2015-04-07 20:11:44 -0400

    Refactored, and allow selection of two commits to diff instead of always against HEAD

--- src/vew.py
+++ src/vew.py
@@ -4,44 +4,23 @@ import subprocess
 import re
 from Queue import LifoQueue
 
-class App(object):
+class ViewManager(object):
+    def __init__(self):
+        self.main_frame = None
+        self.current_view = None
 
-    def run_app(self):
-
-        items = [urwid.Text("foo"),
-                urwid.Text("bar"),
-                urwid.Text("baz")]
-
-        listbox = LogView()
-        listbox.show_key = urwid.Text("Press any key", wrap='clip')
-        self.current_listbox = listbox
-
-        head = urwid.AttrMap(listbox.show_key, 'header')
-        self.top = urwid.Frame(listbox, head, focus_part='body')
-
-        self.loop = urwid.MainLoop(self.top, self.get_pallette(), input_filter=listbox.show_all_input,
+    def start(self):
+        self._loop = urwid.MainLoop(self.main_frame, self.get_pallette(), input_filter=self.current_view.show_all_input,
                                 unhandled_input=self.on_keypress)
-        self.loop.run()
+        self._loop.run()
+
+    def change_view(self, view):
+        self.current_view = view
+        self.main_frame.contents['body'] = ( view, None )
+        self._loop.draw_screen()
 
     def on_keypress(self, input):
-        if (input == 'enter'):
-            # TODO: Move this to LogView.on_keypress. Use LifoQueue in place of self.current_listbox
-            listbox = DiffView(self.current_listbox.content[self.current_listbox.focus_position].commit)
-            listbox.show_key = urwid.Text("Press any key", wrap='clip')
-            self.current_listbox = listbox
-            self.top.contents['body'] = ( listbox, None )
-            self.loop.draw_screen()
-        else:
-            self.current_listbox.on_keypress(input)
-
-    def gui_clear(self):
-        self.loop.widget = urwid.Filler(urwid.Text(u''))
-        self.loop.draw_screen()
-
-    def get_diff(self):
-        diff = GitDiff()
-        response = diff.get()
-        return response
+        self.current_view.on_keypress(input)
 
     def get_pallette(self):
         palette = [('header', 'white', 'white', 'standout'),
@@ -52,12 +31,35 @@ class App(object):
                    ('deleted', 'dark red', 'black')]
         return palette
 
+class App(object):
+
+    def run_app(self):
+
+        items = [urwid.Text("foo"),
+                urwid.Text("bar"),
+                urwid.Text("baz")]
+
+        self.view_manager = ViewManager()
+        view = LogView(self.view_manager)
+        view.show_key = urwid.Text("Press any key", wrap='clip')
+        self.view_manager.current_view = view
+
+        head = urwid.AttrMap(view.show_key, 'header')
+        self.view_manager.main_frame = urwid.Frame(view, head, focus_part='body')
+
+        self.view_manager.start()
+
+
 class ViewController(object):
     def __init__(self):
         self.current_listbox = None
         self.last_listbox_stack = LifoQueue()
 
 class GitView(urwid.ListBox):
+
+    def __init__(self, view_manager, content):
+        self.view_manager = view_manager
+        super(GitView, self).__init__(content)
 
     def show_all_input(self, input, raw):
         self.show_key.set_text("Pressed: " + " ".join([
@@ -119,24 +121,25 @@ class LogLinesBuilder(object):
 
 
 class LogLine(object):
-    
+
     def __init__(self, text, commit):
         self.text = text
         self.commit = commit
 
     def __str__(self):
         return self.text
-    
 
 class LogView(GitView):
 
-    def __init__(self):
+    def __init__(self, view_manager):
         log = self.get()
+
         content = urwid.SimpleListWalker(log)
-
         self.content = content
-        super(LogView, self).__init__(content)
 
+        self.selected_commit = None
+
+        super(LogView, self).__init__(view_manager, content)
 
     def out(self, s):
         self.show_key.set_text(str(s))
@@ -151,21 +154,38 @@ class LogView(GitView):
         return builder.build()
 
     def on_keypress(self, input):
-        super(LogView, self).on_keypress(input)
+        if input == 'enter':
+            if self.selected_commit:
+                listbox = None
+                previously_selected_commit = self.selected_commit
+                newly_selected_commit = self.content[self.focus_position].commit
+                if previously_selected_commit == newly_selected_commit:
+                    listbox = DiffView(self.view_manager, newly_selected_commit, 'HEAD')
+                else:
+                    listbox = DiffView(self.view_manager, previously_selected_commit, newly_selected_commit)
+
+                self.view_manager.change_view(listbox)
+            else: 
+                show_key = self.view_manager.current_view.show_key
+                self.selected_commit = self.content[self.focus_position].commit
+                show_key.set_text("Press enter on another commit, or enter here again to compare to HEAD\nCommit: " + self.selected_commit)
+        else:
+            super(LogView, self).on_keypress(input)
 
 class DiffView(GitView):
 
-    def __init__(self, commit):
-        self.commit = commit
+    def __init__(self, view_manager, first_commit, second_commit):
+        self.first_commit = first_commit
+        self.second_commit = second_commit
         log = self.get()
         content = urwid.SimpleListWalker(log)
 
         self.content = content
-        super(DiffView, self).__init__(content)
+        super(DiffView, self).__init__(view_manager, content)
 
     def get(self):
         diff = subprocess.Popen(
-            "git diff " + self.commit + "..HEAD"
+            "git diff " + self.first_commit + ".." + self.second_commit
             , shell=True, stdout=subprocess.PIPE).stdout.read()
         lines = diff.split('\n')
         return [ self.decorate(line) for line in lines ]
